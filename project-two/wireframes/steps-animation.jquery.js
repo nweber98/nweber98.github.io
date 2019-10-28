f7831632c3051623379c9017ae158db2a8ee3638
Natalie Weber <nweber@nd.edu>
2019-10-28 00:42:35 -0400

    more code my dude

--- project-two/wireframes/steps-animation.jquery.js
+++ project-two/wireframes/steps-animation.jquery.js
@@ -24,12 +24,188 @@ var nextstep = x[2];
 
 nextstep.onclick = function() {
   countnext += 1;
+
+  if (countnext == 1){
+    $(".rotate-lleft").animate({
+        left: '255px',
+    });
+
+    $(".rotate-fright").animate({
+        left: '445px',
+    });
+  }
+ if (countnext == 2){
+    $(".rotate-lleft").animate({
+        left: '185px',
+    });
+
+    $(".rotate-fright").animate({
+        left: '375px',
+    });
+  }
+
+  if (countnext == 3){
+    $(".rotate-lleft").animate({
+        left: '115px',
+    });
+
+    $(".rotate-fright").animate({
+        left: '305px',
+    });
+  }
+  if (countnext == 4){
+    $(".rotate-lleft").animate({
+        left: '185px',
+    });
+
+    $(".rotate-fright").animate({
+        left: '375px',
+    });
+  }
+
+    if (countnext == 5){
+    $(".rotate-lright").animate({
+        left: '115px',
+    });
+
+    $(".rotate-fleft").animate({
+        left: '305px',
+    });
+  }
+    
+   if (countnext == 6){
+    $(".rotate-lright").animate({
+        left: '185px',
+    });
+
+    $(".rotate-fleft").animate({
+        left: '375px',
+    });
+  }
+
+  if (countnext == 7){
+    $(".rotate-lright").animate({
+        left: '255px',
+    });
+
+    $(".rotate-fleft").animate({
+        left: '445px',
+    });
+  }
+  if (countnext == 8){
+    $(".rotate-lright").animate({
+        left: '185px',
+    });
+
+    $(".rotate-fleft").animate({
+        left: '375px',
+    });
+      
+    countnext = 0;
+  }
+  
+  
   nextstep.innerHTML = "click me " + countnext;
 }
 
 previousstep.onclick = function() {
   countprevious += 1;
-  previousstep.innerHTML = "click me " + countprevious;
+    
+  if (countnext == 0){
+        $(".rotate-lright").animate({
+            left: '255px',
+        });
+
+        $(".rotate-fleft").animate({
+            left: '445px',
+        });
+      
+      countnext=7;
+  }
+    
+  if (countnext == 1){
+      $(".rotate-lleft").animate({
+          left:'185px',
+      });
+      
+      $(".rotate-fright").animate({
+          left:'375px',
+      });
+    
+    countnext=0;
+  }
+
+    if (countnext == 2){
+        $(".rotate-lleft").animate({
+            left: '255px',
+        });
+
+        $(".rotate-fright").animate({
+            left: '445px',
+        });
+        
+        countnext=1;
+      }
+     if (countnext == 3){
+        $(".rotate-lleft").animate({
+            left: '185px',
+        });
+
+        $(".rotate-fright").animate({
+            left: '375px',
+        });
+        
+        countnext=2;
+      }
+
+      if (countnext == 4){
+        $(".rotate-lleft").animate({
+            left: '115px',
+        });
+
+        $(".rotate-fright").animate({
+            left: '305px',
+        });
+          
+        countnext=3;
+      }
+      if (countnext == 5){
+
+          $(".rotate-lright").animate({
+            left:'185px',
+          });
+      
+          $(".rotate-fleft").animate({
+              left:'375px',
+          });
+        countnext=4;
+      }
+
+        if (countnext == 6){
+        $(".rotate-lright").animate({
+            left: '115px',
+        });
+
+        $(".rotate-fleft").animate({
+            left: '305px',
+        });
+            
+        countnext=5;
+      }
+
+       if (countnext == 7){
+        $(".rotate-lright").animate({
+            left: '185px',
+        });
+
+        $(".rotate-fleft").animate({
+            left: '375px',
+        });
+        
+        countnext=6;
+      }
+
+  previousstep.innerHTML = "click me " + countnext;
 }
 
 });
